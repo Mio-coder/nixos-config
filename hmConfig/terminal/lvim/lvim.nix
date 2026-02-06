7cbda770a18fab5151bf590f797b4ecd6b4dfcf9
Mio-coder <78543744+Mio-coder@users.noreply.github.com>
2026-02-06 20:44:42 +0100

    flake update

--- hmConfig/terminal/lvim/lvim.nix
+++ hmConfig/terminal/lvim/lvim.nix
@@ -4,6 +4,7 @@
       runtimeDeps = previousAttrs.runtimeDeps ++ [python3 clang-tools];
     }))
     nixd
+    emacs
   ];
   xdg.configFile."lvim/config.lua".source = ./lvim_config.lua;
   xdg.configFile."lvim/patches/nvim-treesitter-patches.patch".source = ./nvim-treesitter-patches.patch;
