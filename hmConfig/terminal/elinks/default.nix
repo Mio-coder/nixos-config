d7cb52b272dd0223ec3ee08070353f7e60531e45
Mio-coder <78543744+Mio-coder@users.noreply.github.com>
2026-02-06 22:01:39 +0100

    some reordering, removed elinks

--- hmConfig/terminal/elinks/default.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    (elinks.override {
-      enablePython = true;
-      python = python3;
-    })
-  ];
-  # TODO: once on stable config, move it in here
-}
--- hmConfig/terminal/shell/shell.nix
+++ hmConfig/terminal/shell/shell.nix
@@ -1,7 +1,6 @@
 _: {
   home.shellAliases = {
     # win = "docker compose --file ~/.config/winapps/compose.yaml";
-    colorgrind = "valgrind -q ./a.out 2>&1 | sed -E \"s/alloc'd/alloc/g; s/==[0-9]+==//g\" | bat -plcpp --no-pager";
     alert = "notify-send --urgency=low -i \"$([ $? = 0 ] && echo terminal || echo error)\" \"$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')\"";
     loadsh = "source ~/.bashrc";
 
@@ -9,9 +8,6 @@ _: {
     mv = "mv -i";
     cp = "cp -i";
     rm = "rm -I";
-
-    ".." = "cd ..";
-    "..." = "cd ../..";
 
     ls = "ls --color=auto";
     ll = "ls -AlF";
